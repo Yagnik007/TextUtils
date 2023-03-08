68e91b5b232fa699a51c32833c43b32b76bb3420
Yagnik007 <yagniktalaviya4122002@outlook.com>
2023-03-08 11:46:18 +0530

    Sample

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,10 +19,10 @@ export default function Navbar(props) {
               <a className="nav-link" href="/">{props.AboutText}</a>
             </li>
           </ul>
-          {/* <form className="d-flex" role="search">
+          <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
             <button className="btn btn-success" type="submit">Search</button>
-          </form> */}
+          </form>
         </div>
       </div>
     </nav>
