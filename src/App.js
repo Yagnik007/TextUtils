57ae7adea6982a1b871efade910d70e54a8b795b
Yagnik007 <yagniktalaviya4122002@gmail.com>
2023-03-20 16:38:56 +0530

    DarkMode Complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import React, { useState } from 'react';
 import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+
+
 function App() {
+  const[mode, setMode] = useState('light');
+
+  const toggleMode = ()=>{
+    if(mode === 'light') 
+    {
+      setMode('dark');
+      document.body.style.backgroundColor = '#740c16';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
     <>
-      <Navbar title = "Text-utils"/>
+      <Navbar title = "TextUtils" mode = {mode} toggleMode={toggleMode}/>
       <div className="container my-3">
-        <TextForm/>
+        <TextForm mode = {mode} toggleMode={toggleMode}/>
       </div>
-      {/* <About/> */}
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState} from 'react';
 
 
 export default function TextForm(props) {
@@ -44,23 +44,25 @@ export default function TextForm(props) {
 
   return (
     <>
-    <div className="container my-6">
+    <div className="container my-3" style = {{color: props.mode === 'dark'? 'white' :'black'}}>
         <h2>Enter your text</h2>
         <div className="mb-3">
-        <textarea className="border border-success border-3 form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <textarea className="border border-danger border-3 form-control" value={text} onChange={handleOnChange} style = {{backgroundColor:props.mode==='dark'?'#4c070e':'white', color: props.mode === 'dark'? 'white' :'black'}} id="myBox" rows="8"></textarea>
         </div>
         <h5>Your text has <b>{calculateWords()}</b> words, <b>{text.length}</b> characters and takes <b>{Math.round(0.008 * calculateWords() *100)/100}</b> minutes read!!</h5>
-        <div className="btn btn-success my-2 mx-2" onClick={handleUpClick}>Convert-to-uppercase</div>
-        <div className="btn btn-success my-2 mx-2" onClick={handleLoClick}>Convert-to-lowercase</div>
-        <div className="btn btn-success my-2 mx-2" onClick={copyToClipboard}>Copy-Text</div>
-        <div className="btn btn-success my-2 mx-2" onClick={handleExtraSpaces}>Remove-Extra-Spaces</div>
+        <div className="btn btn-danger my-2 mx-2" onClick={handleUpClick}>Convert-to-uppercase</div>
+        <div className="btn btn-danger my-2 mx-2" onClick={handleLoClick}>Convert-to-lowercase</div>
+        <div className="btn btn-danger my-2 mx-2" onClick={copyToClipboard}>Copy-Text</div>
+        <div className="btn btn-danger my-2 mx-2" onClick={handleExtraSpaces}>Remove-Extra-Spaces</div>
     </div>
-    <div className="container my-10">
+    <div className="container my-3" style = {{color: props.mode === 'dark'? 'white' :'black'}}>
       <h2 className="preview">Preview</h2>
       <div className="mb-3">
-        <textarea className="border border-dark form-control" value={text} id="myBox" rows="8"></textarea>
+        <textarea className={`border border-${props.mode === 'dark'? 'white':'success'} form-control`} value={text.length>0 ? text : "Enter some Text for the Preview"}  style = {{backgroundColor: props.mode==='dark'?'#4c070e':'white', color: props.mode === 'dark'? 'white' :'black'}} id="myBox" rows="8"></textarea>
       </div>
     </div>
     </>
   )
 }
+
+
