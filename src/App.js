9029c3b5255215add030a9d8c707d21be4ed2172
Yagnik007 <yagniktalaviya4122002@gmail.com>
2023-03-21 14:53:22 +0530

    Title and Favicon

--- src/App.js
+++ src/App.js
@@ -1,35 +1,54 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 // import About from './components/About';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-
+import Alert from "./components/Alert";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
-  const[mode, setMode] = useState('light');
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-  const toggleMode = ()=>{
-    if(mode === 'light') 
-    {
-      setMode('dark');
-      document.body.style.backgroundColor = '#740c16';
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500)
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#740c16";
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "Textutils - DarkMode";
+      // setInterval(()=>{
+      //   document.title = "Textutils is Amazing";
+      // }, 2000);
+      // setInterval(()=>{
+      //   document.title = "Install Textutils now!";
+      // }, 1500);
+    } 
+    else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+      document.title = "Textutils - LightMode";
     }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
-    }
-  }
+  };
 
   return (
     <>
-      <Navbar title = "TextUtils" mode = {mode} toggleMode={toggleMode}/>
+      <Navbar title="TEXTUTILS" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm mode = {mode} toggleMode={toggleMode}/>
+        <TextForm showAlert={showAlert} mode={mode} toggleMode={toggleMode} />
       </div>
     </>
   );
 }
 
 export default App;
-
-
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,29 +6,36 @@ export default function TextForm(props) {
   {
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted to UPPERCASE!!", "success");
   }
 
   const handleLoClick = () =>
   {
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Converted to lowercase!!", "success");
   }
 
   const handleOnChange = (event) =>
   {
     setText(event.target.value)
   }
+
   const [text, setText] = useState("");
 
   const copyToClipboard = () =>
   {
     navigator.clipboard.writeText(text);
+    props.showAlert("Text Copied to Clipboard!!", "success");
+
   }
 
   const handleExtraSpaces = () => 
   {
     let newText = text.split(/[ ]+/);
-    setText(newText.join(" "))
+    setText(newText.join(" "));
+    props.showAlert("Extra Spaces Removed!!", "success");
+
   }
 
   const calculateWords = () => {
@@ -50,10 +57,10 @@ export default function TextForm(props) {
         <textarea className="border border-danger border-3 form-control" value={text} onChange={handleOnChange} style = {{backgroundColor:props.mode==='dark'?'#4c070e':'white', color: props.mode === 'dark'? 'white' :'black'}} id="myBox" rows="8"></textarea>
         </div>
         <h5>Your text has <b>{calculateWords()}</b> words, <b>{text.length}</b> characters and takes <b>{Math.round(0.008 * calculateWords() *100)/100}</b> minutes read!!</h5>
-        <div className="btn btn-danger my-2 mx-2" onClick={handleUpClick}>Convert-to-uppercase</div>
-        <div className="btn btn-danger my-2 mx-2" onClick={handleLoClick}>Convert-to-lowercase</div>
-        <div className="btn btn-danger my-2 mx-2" onClick={copyToClipboard}>Copy-Text</div>
-        <div className="btn btn-danger my-2 mx-2" onClick={handleExtraSpaces}>Remove-Extra-Spaces</div>
+        <div className="btn btn-danger my-2 mx-2" onClick={handleUpClick}>UpperCase</div>
+        <div className="btn btn-danger my-2 mx-2" onClick={handleLoClick}>LowerCase</div>
+        <div className="btn btn-danger my-2 mx-2" onClick={copyToClipboard}>Copy</div>
+        <div className="btn btn-danger my-2 mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</div>
     </div>
     <div className="container my-3" style = {{color: props.mode === 'dark'? 'white' :'black'}}>
       <h2 className="preview">Preview</h2>
